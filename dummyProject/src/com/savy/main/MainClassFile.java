dff2c6f357cfa00433976b87a9ed4563fc4b8e82
sourav <sourav@10.124.92.178>
2017-04-18 21:10:21 +0530

    initial copy

---
+++ dummyProject/src/com/savy/main/MainClassFile.java
@@ -0,0 +1,9 @@
+package com.savy.main;
+
+public class MainClassFile {
+
+	public static void main(String[] args) {
+		MainClassFile mainClassFile = new MainClassFile();
+		System.out.println("Greetings Earthlings!!!");
+	}
+}
