332527b6c3e2aa2e6a1d5cee13c92d3716142de6
sourav <sourav@10.124.92.178>
2017-04-18 21:17:13 +0530

    another updated copy

--- dummyProject/src/com/savy/main/MainClassFile.java
+++ dummyProject/src/com/savy/main/MainClassFile.java
@@ -3,7 +3,10 @@ package com.savy.main;
 public class MainClassFile {
 
 	public static void main(String[] args) {
-		MainClassFile mainClassFile = new MainClassFile();
+		MainClassFile.printLine();
+	}
+	
+	public static void printLine() {
 		System.out.println("Greetings Earthlings!!!");
 	}
 }
